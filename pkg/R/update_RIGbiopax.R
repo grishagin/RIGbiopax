38e48601d3d2d10dac9e369b24227c3aaa1e23d3
Ivan Grishagin <Ivan Grishagin>
2016-08-18 16:11:44 -0400

    Minor bugfix

--- pkg/R/update_RIGbiopax.R
+++ pkg/R/update_RIGbiopax.R
@@ -2,5 +2,6 @@ update_RIGbiopax <-
     function () 
     {
         unloadNamespace(ns = "RIGbiopax")
+        remove.packages("RIGbiopax")
         devtools::install_github("grishagin/RIGbiopax", subdir = "pkg")
     }
