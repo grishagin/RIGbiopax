c4be762626061e4162b14992bc7f20420904141c
Ivan Grishagin <Ivan Grishagin>
2016-08-18 15:29:06 -0400

    Minor bugfix

--- pkg/R/writeBiopax_Rancho.R
+++ pkg/R/writeBiopax_Rancho.R
@@ -5,22 +5,27 @@ writeBiopax_Rancho<-
                   ,overwrite = FALSE
                   ,biopaxlevel=3
                   ,namespaces=NULL) {
+            
+                if (!biopaxlevel %in% c(2,3)){
+                    stop("writeBiopax_Rancho: Incorrect biopax level specified. Aborting.")
+                }
 
-                if (biopaxlevel==2){
+            
+                if (is.null(namespaces)){
+                    if (biopaxlevel==2){
+                        message("Using default namespaces for biopax level 2.")
                         namespaces<-list(rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#", 
                                          bp = "http://www.biopax.org/release/biopax-biopaxlevel2.owl#", 
                                          rdfs = "http://www.w3.org/2000/01/rdf-schema#", owl = "http://www.w3.org/2002/07/owl#", 
                                          xsd = "http://www.w3.org/2001/XMLSchema#")
-                } else if (biopaxlevel==3){
+                    } else if (biopaxlevel==3){
+                        message("Using default namespaces for biopax level 3.")
                         namespaces<-list(rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#", 
                                          bp = "http://www.biopax.org/release/biopax-biopaxlevel3.owl#", 
                                          rdfs = "http://www.w3.org/2000/01/rdf-schema#", owl = "http://www.w3.org/2002/07/owl#", 
                                          xsd = "http://www.w3.org/2001/XMLSchema#")
-                } else if (is.null(biopaxlevel)){
-                        if(is.null(namespaces)){
-                                stop("Since you have provided a biopax biopaxlevel NULL, you have to specify namespaces. Stopping.")
-                        }
-                }
+                    }
+                } 
                 
                 
                 if (file.exists(file) & !overwrite) {
