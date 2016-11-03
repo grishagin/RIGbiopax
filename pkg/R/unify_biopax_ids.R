804a5d7cca9f279c2fdf1b2eaa122c26b5e97e17
Ivan Grishagin <Ivan Grishagin>
2016-11-03 15:14:57 -0400

    bugfix and small update

--- pkg/R/unify_biopax_ids.R
+++ pkg/R/unify_biopax_ids.R
@@ -1,17 +1,26 @@
 unify_biopax_ids<-
     function(biopax_dt
              ,idtag=NULL
-             ,exclude_id_pattern="bioplanet"){
+             ,exclude_id_pattern="bioplanet"
+             ,exclude_class="Pathway"){
         #standardizes representation of all biopax ids
         #take unique classes with ids combinations
+        #doesn't touch ids with exclude_id_pattern only OF class exclude_class
         st<-Sys.time()
         class_id<-
             biopax_dt %>%
             dplyr::select(class,id) %>%
             #exclude instances with ids featuring a given pattern
-            subset(!grepl(exclude_id_pattern
-                          ,id)) %>%
+            #AND belonging to the class to be excluded
+            filter(!(grepl(exclude_id_pattern
+                           ,id) &
+                         class %in% exclude_class)) %>%
             unique
+        
+        if (nrow(class_id)<1){
+            message("unify_biopax_ids: no ids to replace!")
+            return(biopax_dt)
+        }
         #some entities refer to instances of multiple different classes
         #in that case, we'll replace class with "PhysicalEntity"
         dupl_ids<-
@@ -32,13 +41,18 @@ unify_biopax_ids<-
             biopax_dt$id %>%
             mapvalues(from = class_id$id
                       ,to = class_id$newid)
+        #remove ids that are not referenced in the original dataframe
+        class_id<-
+            class_id %>%
+            filter(id %in% biopax_dt$property_attr_value)
+
         #replace prop attr values (references)
         biopax_dt$property_attr_value<-
             biopax_dt$property_attr_value %>%
             mapvalues(from = class_id$id
                       ,to = class_id$newid)
         et<-Sys.time()
-        print(et-st)
+        #print(et-st)
         
         
         return(biopax_dt)
