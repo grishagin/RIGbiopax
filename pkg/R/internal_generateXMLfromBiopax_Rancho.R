6d9614189392e1e656136d2db4c81a2b974e33f9
Ivan Grishagin <Ivan Grishagin>
2017-03-01 12:18:45 -0500

    Version 3.9

--- pkg/R/internal_generateXMLfromBiopax_Rancho.R
+++ pkg/R/internal_generateXMLfromBiopax_Rancho.R
@@ -1,6 +1,6 @@
 internal_generateXMLfromBiopax_Rancho<-
     function (biopax
-              ,filename=NULL
+              ,filename="default"
               ,namespaces = namespaces
               ,biopaxlevel=3
               ,verbose=TRUE) 
@@ -13,22 +13,22 @@ internal_generateXMLfromBiopax_Rancho<-
         }
         
         #assemble the xml file header
-        output<-
+        output_header<-
             '<?xml version="1.0" encoding="ISO-8859-1"?>\n<rdf:RDF '
         #add namespaces' definitions
-        output<-
+        output_header<-
             paste0("xmlns:"
                    ,names(namespaces)
                    ,'="'
                    ,namespaces
                    ,'"') %>%
             paste(collapse="\n") %>%
-            paste0(output
+            paste0(output_header
                    ,"\n"
                    ,.
                    ,">\n")
         #add ontology definitions
-        output<-
+        output_header<-
             paste0('<owl:imports rdf:resource="'
                    ,level_url
                    ,'"/>\n'
@@ -37,75 +37,76 @@ internal_generateXMLfromBiopax_Rancho<-
                    ,date()
                    ,' using the Rancho-modified rBiopaxParser package.</rdfs:comment>\n'
             ) %>%
-            paste0(output
+            paste0(output_header
                    ,'<owl:Ontology rdf:about="">\n'
                    ,.
-                   ,'</owl:Ontology>\n')
+                   ,'</owl:Ontology>')
         
         #add combined class/id id to the dataframe
         biopax$dt<-
-            biopax$dt %>%
-            mutate(classid=paste0(class,id)) %>%
-            arrange(classid)
-        message("Combined ids prepared.")
+            biopax$dt[,.(classid=paste0(class,id))] %>% 
+            cbind(biopax$dt) %>% 
+            .[order(classid)]
+        
+        #message("Combined ids prepared.")
         
         #open/close tag dataframe for each combined id
         instance_class_id<- 
             biopax$dt %>% 
-            dplyr::select(class,id,classid) %>%
-            unique %>%
-            mutate(opentag=
-                       paste0('<bp:'
-                              ,class
-                              ,' rdf:about="'
-                              ,id
-                              ,'">\n'
-                       )
-                   ,closetag=
-                       paste0('</bp:'
-                              ,class
-                              ,'>\n'
-                       )
-            )
+            .[,.(classid
+                 ,opentag=
+                     paste0('<bp:'
+                            ,class
+                            ,' rdf:about="'
+                            ,id
+                            ,'">\n'
+                     )
+                 ,closetag=
+                     paste0('</bp:'
+                            ,class
+                            ,'>'
+                     ))] %>% 
+            unique
         
         #now prepare tags for each instance in the biopax df
-        #first, for those with have property value
+        #first, for those which have property value
         instance_df_pv<-
             biopax$dt %>%
-            filter(property_value!="") %>%
-            mutate(tag=
-                       paste0('<bp:'
-                              ,property
-                              ,' '
-                              ,property_attr
-                              ,'="'
-                              ,property_attr_value
-                              ,'">'
-                              ,property_value
-                              ,'</bp:'
-                              ,property
-                              ,'>\n'
-                       )
-            )
+            .[property_value!=""
+              ,.(classid
+                 ,tag=
+                     paste0('<bp:'
+                            ,property
+                            ,' '
+                            ,property_attr
+                            ,'="'
+                            ,property_attr_value
+                            ,'">'
+                            ,property_value
+                            ,'</bp:'
+                            ,property
+                            ,'>\n'
+                     ))]
+            
         #... and then for those who don't
         instance_df_no_pv<-
             biopax$dt %>%
-            filter(property_value=="") %>%
-            mutate(tag=
-                       paste0('<bp:'
-                              ,property
-                              ,' '
-                              ,property_attr
-                              ,'="'
-                              ,property_attr_value
-                              ,'"/>\n'
-                       )
-            )
+            .[property_value==""
+              ,.(classid
+                 ,tag=
+                     paste0('<bp:'
+                            ,property
+                            ,' '
+                            ,property_attr
+                            ,'="'
+                            ,property_attr_value
+                            ,'"/>\n'))]
+        
         #combine and turn in to a data table
         instance_df<-
             instance_df_pv %>%
-            rbind.data.frame(instance_df_no_pv) %>%
-            as.data.table
+            rbind(instance_df_no_pv) %>% 
+            .[order(classid)]
         
         message("All tags for interaction components and each instance were prepared.")
         
@@ -114,126 +115,39 @@ internal_generateXMLfromBiopax_Rancho<-
         
         message("Adding instances' tags to referencing components' tags...")
         message("It should take roughly "
-                ,round(x = 0.002*nrow(instance_class_id)
+                ,round(x = 1e-5*nrow(instance_class_id)
                        ,digits = 0)
                 ," seconds.")
         
         st<-Sys.time()
         #for each unique class/id combo find correponding referenced instances
         #and merge their corresponding tags into one line
-        vect_by_classid_kids<-
-            sapply(1:nrow(instance_class_id)
-                   ,FUN=function(clid){
-                       instance_df[instance_class_id$classid[clid],tag]
-                   }) %>%
-            lapply(paste,collapse="")
+        df_by_classid_kids<-
+            instance_df[,.(kidstags=paste(tag,collapse="")),by=classid]
+        
         et<-Sys.time()
         message("Tag addition is over. It took "
                 ,round(x = difftime(et,st,units="secs")
-                       ,digits = 0)
+                       ,digits = 5)
                 ," seconds.")
         
         #merge open tags with referenced instances tag strings and with close tags
-        vect_by_classid_full<-
-            paste(instance_class_id$opentag
-                  ,vect_by_classid_kids
-                  ,instance_class_id$closetag
-                  ,sep = "")
+        df_by_classid_full<-
+            data.table(opentag=instance_class_id$opentag
+                       ,kidstag=df_by_classid_kids$kidstags
+                       ,closetag=instance_class_id$closetag) 
+           
         #merge everything together into one string
         output<-
-            vect_by_classid_full %>%
-            paste(collapse="") %>%
-            paste0(output
-                   ,.
-                   ,"</rdf:RDF>")
-        
-        if (is.null(filename)) {
-            file<-paste0(Sys.Date()
-                         ,"_generated_biopax.owl")
-        }
-        #write to file
-        write(output
-              ,file=filename)
+            df_by_classid_full %>% 
+            .[,.(paste0(opentag
+                        ,kidstag
+                        ,closetag))] %>% 
+            unlist %>% 
+            c(output_header
+              ,.
+              ,"</rdf:RDF>")
+    
         
         return(output)
     }
-
-# internal_generateXMLfromBiopax_Rancho<-
-#     function (biopax
-#               ,namespaces = namespaces
-#               ,verbose = TRUE
-#               ,biopaxlevel=3) 
-#     {
-#         
-#         if (biopaxlevel==2){
-#             level_url<-"http://www.biopax.org/release/biopax-level2.owl"
-#         } else if (biopaxlevel==3){
-#             level_url<-"http://www.biopax.org/release/biopax-level3.owl"
-#         }
-#         
-#         
-#         d = suppressWarnings(XML::xmlTree("rdf:RDF"
-#                                           ,namespaces = namespaces))
-#         d$addNode("Ontology"
-#                   ,namespace = "owl"
-#                   ,attrs = c(`rdf:about` = "")
-#                   ,close = FALSE)
-#         d$addNode("imports"
-#                   ,namespace = "owl"
-#                   ,attrs = c(`rdf:resource` = level_url))
-#         d$addNode("comment"
-#                   ,paste("BioPAX output created"
-#                          ,date()
-#                          ,"using the Rancho-modified rBiopaxParser package.")
-#                   ,namespace = "rdfs"
-#                   ,attrs = c(`rdf:datatype` = "http://www.w3.org/2001/XMLSchema#string"))
-#         
-#         d$closeTag()
-#         instanceList<- 
-#             unique(biopax$dt[, list(class, id)])
-#         count = 1
-#         
-#         #for (i in 1:dim(instanceList)[1]) {
-#         for (i in 1:2000) {
-#             instance = biopax$dt[class == instanceList[i]$class & 
-#                                      id == instanceList[i]$id, ]
-#             d$addNode(as.character(instance[1]$class)
-#                       ,namespace = "bp"
-#                       #,attrs = c(`rdf:ID` = as.character(instance[1]$id))
-#                       #rdf:ID does not seem to work for SBML conversion -- yields empty document
-#                       #in wiki, rdf:ID is replaced with rdf:about -- seems to work
-#                       #inexplicably, original biocarta file (which has rdf:ID) works though
-#                       ,attrs = c(`rdf:about` = as.character(instance[1]$id))
-#                       ,close = FALSE)
-#             
-#             for (p in 1:dim(instance)[1]) {
-#                 attrs = c(as.character(instance[p]$property_attr_value))
-#                 names(attrs) = as.character(instance[p]$property_attr)
-#                 
-#                 if (nchar(as.character(instance[p]$property_value)) > 0) {
-#                     d$addNode(as.character(instance[p]$property), 
-#                               as.character(instance[p]$property_value)
-#                               ,namespace = "bp"
-#                               ,attrs = attrs)
-#                 }
-#                 else {
-#                     d$addNode(as.character(instance[p]$property)
-#                               ,namespace = "bp"
-#                               ,attrs = attrs)
-#                 }
-#             }
-#             d$closeTag()
-#             if (verbose) {
-#                 if (i%%1000 == 0) 
-#                     message(paste0("INFO: Wrote instance nr "
-#                                    ,i
-#                                    ," of "
-#                                    ,dim(instanceList)[1]
-#                                    ," with id "
-#                                    ,instance$id[1]
-#                                    ,"."))
-#                 count = count + 1
-#             }
-#         }
-#         d
-#     }
--- pkg/R/writeBiopax_Rancho.R
+++ pkg/R/writeBiopax_Rancho.R
@@ -1,6 +1,6 @@
 writeBiopax_Rancho<-
     function (biopax
-              ,filename = NULL
+              ,filename = "default"
               ,overwrite = FALSE
               ,biopaxlevel=3
               ,namespaces=NULL) {
@@ -38,76 +38,20 @@ writeBiopax_Rancho<-
         
         RIGbiopax:::internal_checkValidity_Rancho(biopax)
         
-        d <-
+        output <-
             internal_generateXMLfromBiopax_Rancho(biopax = biopax
                                                   ,filename=filename
                                                   ,namespaces = namespaces
                                                   ,biopaxlevel = biopaxlevel
                                                   ,verbose=verbose)
+        if (filename=="default") {
+            filename<-
+                paste0(Sys.Date()
+                       ,"_generated_biopax.owl")
+        }
+        #write to file
+        writeLines(output
+              ,con = filename
+              ,useBytes = TRUE)
         return()
     }
-# writeBiopax_Rancho<-
-#         function (biopax
-#                   ,file = NULL
-#                   ,verbose = TRUE
-#                   ,overwrite = FALSE
-#                   ,biopaxlevel=3
-#                   ,namespaces=NULL
-#                   ,encoding="UTF-8") {
-#             
-#                 if (!biopaxlevel %in% c(2,3)){
-#                     stop("writeBiopax_Rancho: Incorrect biopax level specified. Aborting.")
-#                 }
-#             
-#                 if (is.null(file)){
-#                     file<-paste(Sys.Date()
-#                                 ,"new_biopax.owl")
-#                 }
-# 
-#             
-#                 if (is.null(namespaces)){
-#                     if (biopaxlevel==2){
-#                         message("Using default namespaces for biopax level 2.")
-#                         namespaces<-list(rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-#                                          ,bp = "http://www.biopax.org/release/biopax-level2.owl#" 
-#                                          ,rdfs = "http://www.w3.org/2000/01/rdf-schema#"
-#                                          ,owl = "http://www.w3.org/2002/07/owl#" 
-#                                          ,xsd = "http://www.w3.org/2001/XMLSchema#")
-#                     } else if (biopaxlevel==3){
-#                         message("Using default namespaces for biopax level 3.")
-#                         namespaces<-list(rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-#                                          ,bp = "http://www.biopax.org/release/biopax-level3.owl#"
-#                                          ,rdfs = "http://www.w3.org/2000/01/rdf-schema#"
-#                                          ,owl = "http://www.w3.org/2002/07/owl#"
-#                                          ,xsd = "http://www.w3.org/2001/XMLSchema#")
-#                     }
-#                 } 
-#                 
-#                 
-#                 if (file.exists(file) & !overwrite) {
-#                         stop(paste("Error: File ", file, " already exists.", 
-#                                    sep = ""))
-#                 }
-#                 
-#                 internal_checkValidity_Rancho(biopax)
-#                 
-#                 d <-
-#                     internal_generateXMLfromBiopax_Rancho(biopax = biopax
-#                                                           ,namespaces = namespaces
-#                                                           ,verbose = verbose
-#                                                           ,biopaxlevel = biopaxlevel)
-#                 
-#                 
-#                 if (is.null(file)) {
-#                     file<-paste0(Sys.Date()
-#                                  ,"_generated_biopax.owl")
-#                 }
-#                 #cannot use internal XML writing method
-#                 #because addition of encoding or changing of prefix
-#                 #messes up indents (results in one-line output)
-#                 writeLines(XML::saveXML(d$value()
-#                              ,encoding=encoding
-#                              )
-#                            ,con = file)
-#                 
-#         }
